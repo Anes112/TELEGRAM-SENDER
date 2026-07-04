6b718193eb14c55d963f89d36043a36c57a9d236
prasdriann-a11y <prasdriann-a11y@users.noreply.github.com>
2026-07-05 02:19:44 +0700

    prompt android ip in desktop launcher

--- Buka Panel Android.bat
+++ Buka Panel Android.bat
@@ -1,4 +1,8 @@
 @echo off
 cd /d "%~dp0"
-python desktop_app.py --remote http://192.168.50.159:5174
+set DEFAULT_IP=192.168.50.159
+set /p ANDROID_IP=Masukkan IP Android [%DEFAULT_IP%]: 
+if "%ANDROID_IP%"=="" set ANDROID_IP=%DEFAULT_IP%
+echo Membuka panel dari http://%ANDROID_IP%:5174
+python desktop_app.py --remote http://%ANDROID_IP%:5174
 pause
